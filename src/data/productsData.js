806932fad6b51c6a6f87ec071034ab9ba3c82b7f
FacundoBisio <facubisio433@gmail.com>
2025-10-02 16:23:04 -0300

    organizacion de carpetas public + yerbas

--- src/data/productsData.js
+++ src/data/productsData.js
@@ -6,7 +6,7 @@ const productsData = {
         id: 'camionero-1',
         name: 'Camionero Criollo Con Base',
         price: '17999',
-        image: '/images/CamioneroCriolloConBase.webp',
+        image: '/images/mates/camioneros/CamioneroCriolloConBase.webp',
         href: 'CamioneroCriolloConBase',
         stock: 0
       },
@@ -14,7 +14,7 @@ const productsData = {
         id: 'camionero-2',
         name: 'Camionero De Algarrobo',
         price: '17999',
-        image: '/images/CamioneroDeAlgarrobo.webp',
+        image: '/images/mates/camioneros/CamioneroDeAlgarrobo.webp',
         href: 'CamioneroDeAlgarrobo',
         stock: 0
       }
@@ -24,7 +24,7 @@ const productsData = {
         id: 'imperial-1',
         name: 'Imperial',
         price: '29999',
-        image: '/images/Imperial.webp',
+        image: '/images/mates/imperiales/Imperial.webp',
         href: 'Imperial',
         stock: 4
       },
@@ -32,7 +32,7 @@ const productsData = {
         id: 'imperial-2',
         name: 'Imperial Algarrobo',
         price: '23999',
-        image: '/images/ImperialAlgarrobo.webp',
+        image: '/images/mates/imperiales/ImperialAlgarrobo.webp',
         href: 'ImperialAlgarrobo',
         stock: 2
       },
@@ -40,7 +40,7 @@ const productsData = {
         id: 'imperial-3',
         name: 'Imperial Con Base',
         price: '34999',
-        image: '/images/ImperialConBase.webp',
+        image: '/images/mates/imperiales/ImperialConBase.webp',
         href: 'ImperialConBase',
         stock: 2
       },
@@ -48,7 +48,7 @@ const productsData = {
         id: 'imperial-4',
         name: 'Imperial Cincelado Con Bolitas',
         price: '41999',
-        image: '/images/ImperialCinceladoConBolitas.webp',
+        image: '/images/mates/imperiales/ImperialCinceladoConBolitas.webp',
         href: 'ImperialCinceladoConBolitas',
         stock: 2
       },
@@ -56,7 +56,7 @@ const productsData = {
         id: 'imperial-5',
         name: 'Imperial Cuero Crudo',
         price: '41999',
-        image: '/images/ImperialCueroCrudo.webp',
+        image: '/images/mates/imperiales/ImperialCueroCrudo.webp',
         href: 'ImperialCueroCrudo',
         stock: 1
       },
@@ -66,7 +66,7 @@ const productsData = {
         id: 'torpedo-1',
         name: 'Torpedo Cincelado',
         price: '29999',
-        image: '/images/TorpedoCincelado.webp',
+        image: '/images/mates/torpedos/TorpedoCincelado.webp',
         href: 'TorpedoCincelado',
         stock: 0
       },
@@ -74,7 +74,7 @@ const productsData = {
         id: 'torpedo-2',
         name: 'Torpedo Cincelado Con Bolitas',
         price: '39500',
-        image: '/images/TorpedoCinceladoConBolitas.webp',
+        image: '/images/mates/torpedos/TorpedoCinceladoConBolitas.webp',
         href: 'TorpedoCinceladoConBolitas',
         stock: 1
       },
@@ -82,7 +82,7 @@ const productsData = {
         id: 'torpedo-3',
         name: 'Torpedo base redonda Cuero Labrado',
         price: '34999',
-        image: '/images/TorpedoConBaseCueroTrabajado.webp',
+        image: '/images/mates/torpedos/TorpedoConBaseCueroTrabajado.webp',
         href: 'TorpedoConBaseCueroTrabajado',
         stock: 1
       },
@@ -90,7 +90,7 @@ const productsData = {
         id: 'torpedo-4',
         name: 'Torpedo base redonda Virola Cincelada',
         price: '36999',
-        image: '/images/TorpedoConBaseCincelado.webp',
+        image: '/images/mates/torpedos/TorpedoConBaseCincelado.webp',
         href: 'TorpedoConBaseCincelado',
         stock: 1
       },
@@ -98,7 +98,7 @@ const productsData = {
         id: 'torpedo-5',
         name: 'Torpedo Cuero Crudo',
         price: '41999',
-        image: '/images/TorpedoCueroCrudo.webp',
+        image: '/images/mates/torpedos/TorpedoCueroCrudo.webp',
         href: 'TorpedoCueroCrudo',
         stock: 1
       }
@@ -109,30 +109,117 @@ const productsData = {
       id: 'termo-1',
       name: 'Termo Media Manija 1L Plateado/Negro',
       price: '25000',
-      image: '/images/mediamanija.webp',
+      image: '/images/termos/mediamanija.webp',
       stock: 3
     }
   ],
   YERBAS: [
     {
-      id: 'yerba-1',
-      name: 'Yerba Playadito 1kg',
-      price: '4500',
-      image: '/images/Playadito.webp',
-      stock: 2
-    },
-    {
       id: 'yerba-2',
       name: 'Yerba Canarias 1kg',
-      price: '10000',
-      image: '/images/Canarias.webp',
+      price: '10500',
+      image: '/images/yerbas/Canarias.webp',
       stock: 5
     },
     {
       id: 'yerba-3',
       name: 'Yerba Baldo 1kg',
-      price: '10800',
-      image: '/images/Baldo.webp',
+      price: '10000',
+      image: '/images/yerbas/Baldo.webp',
+      stock: 5
+    }
+    ,
+    {
+      id: 'yerba-4',
+      name: 'Yerba Baldo 500g',
+      price: '5500',
+      image: '/images/yerbas/baldo500.webp',
+      stock: 5
+    }
+    ,
+    {
+      id: 'yerba-5',
+      name: 'Yerba Baldo 5kg',
+      price: '46000',
+      image: '/images/yerbas/baldo5kg.webp',
+      stock: 5
+    }
+    ,
+    {
+      id: 'yerba-6',
+      name: 'Yerba Sara 1kg',
+      price: '10000',
+      image: '/images/yerbas/sara.webp',
+      stock: 5
+    }
+    ,
+    {
+      id: 'yerba-7',
+      name: 'Yerba Sara Suave 1kg',
+      price: '10000',
+      image: '/images/yerbas/saraSuave.webp',
+      stock: 5
+    }
+    ,
+    {
+      id: 'yerba-8',
+      name: 'Yerba Rei Verde Tradicional 1kg',
+      price: '7500',
+      image: '/images/yerbas/reiverde.webp',
+      stock: 5
+    }
+    ,
+    {
+      id: 'yerba-9',
+      name: 'Yerba Rei Verde Clasica 1kg',
+      price: '7000',
+      image: '/images/yerbas/reiverdeClasica.webp',
+      stock: 5
+    }
+    ,
+    {
+      id: 'yerba-10',
+      name: 'Yerba Rei Verde Compuesta 500g',
+      price: '4500',
+      image: '/images/yerbas/reiverdeCompuesta.webp',
+      stock: 5
+    }
+    ,
+    {
+      id: 'yerba-11',
+      name: 'Yerba Rei Verde Premium 1kg',
+      price: '9000',
+      image: '/images/yerbas/reiverdePremium.webp',
+      stock: 5
+    }
+    ,
+    {
+      id: 'yerba-12',
+      name: 'Yerba Rei Verde Padron Argentino 500g',
+      price: '4000',
+      image: '/images/yerbas/reiverdeRoja.webp',
+      stock: 5
+    }
+    ,
+    {
+      id: 'yerba-13',
+      name: 'Yerba Libre 1kg',
+      price: '6000',
+      image: '/images/yerbas/libre.webp',
+      stock: 5
+    },
+    {
+      id: 'yerba-14',
+      name: 'Yerba Cosmico 500g',
+      price: '4500',
+      image: '/images/yerbas/cosmico.webp',
+      stock: 5
+    },
+    {
+      id: 'yerba-15',
+      name: 'Yerba Verdecita 1kg',
+      price: '8000',
+      image: '/images/yerbas/verdecita.webp',
       stock: 5
     }
   ],
@@ -141,28 +228,28 @@ const productsData = {
       id: 'bombilla-1',
       name: 'Bombilla Pico de loro',
       price: '7000',
-      image: '/images/Bombilla.webp',
+      image: '/images/bombillas/Bombilla.webp',
       stock: 5
     },
     {
       id: 'bombilla-2',
       name: 'Bombilla Trenzada',
       price: '8000',
-      image: '/images/BombillaTrenzada.webp',
+      image: '/images/bombillas/BombillaTrenzada.webp',
       stock: 5
     },
     {
       id: 'bombilla-3',
       name: 'Bombilla con Apliques',
       price: '8000',
-      image: '/images/BombillaApliques.webp',
+      image: '/images/bombillas/BombillaApliques.webp',
       stock: 5
     },
     {
       id: 'bombilla-4',
       name: 'Bombillon Trenzado',
       price: '22500',
-      image: '/images/Bombillon.webp',
+      image: '/images/bombillas/Bombillon.webp',
       stock: 1
     }
   ],
@@ -171,14 +258,14 @@ const productsData = {
       id: 'accesorios-1',
       name: 'Yerberas',
       price: '4000',
-      image: '/images/Yerbera.webp',
+      image: '/images/accesorios/Yerbera.webp',
       stock: 5
     },
     {
       id: 'accesorios-3',
       name: 'Canasta de Cuero',
       price: '40000',
-      image: '/images/canastas.webp',
+      image: '/images/accesorios/canastas.webp',
       stock: 2
     }
   ],
