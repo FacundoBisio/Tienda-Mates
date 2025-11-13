3bd82875d83ee22e96433d84d3c28ec4d2184884
FacundoBisio <facubisio433@gmail.com>
2025-11-13 11:11:52 -0300

    aclaraciones

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,6 +1,7 @@
 // Icons.jsx
 import React from 'react'
 
+// Iconose sociales para el footer
 export function RedesSociales () {
   return (
     <div className="flex justify-center space-x-6 mt-6 text-[#692904] text-2xl">
@@ -88,7 +89,7 @@ export function RedesSociales () {
 
 
 
-
+// carrito del producto
 export function AddToCartIcon () {
     return (
       <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' strokeWidth='1' stroke='currentColor' fill='none' strokeLinecap='round' strokeLinejoin='round'>
